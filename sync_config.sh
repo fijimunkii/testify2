7ee2c91f2361621def55da99056c9d6233b48dde
Harrison Powers <harrisonpowers@gmail.com>
2018-04-30 18:06:36 -0400

    Switch to cp

--- sync_config.sh
+++ sync_config.sh
@@ -9,5 +9,5 @@ if [ -z ${S3_BUCKET+x} ]; then
 else
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   aws s3 sync s3://$S3_BUCKET/cert $DIR/cert >/dev/null 2>&1 || echo "cert sync failed"
-  aws s3 sync s3://$S3_BUCKET/config.json $DIR/config.json >/dev/null 2>&1 || echo "config sync failed"
+  aws s3 cp s3://$S3_BUCKET/config.json $DIR/config.json >/dev/null 2>&1 || echo "config sync failed"
 fi
