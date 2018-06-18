15eb61e1acc78ecdc75489badfadae94a87c6f59
Harrison Powers <harrisonpowers@gmail.com>
2018-06-18 10:58:37 -0400

    Remove data

--- testify.js
+++ testify.js
@@ -65,7 +65,6 @@ async function testify(req, res) {
       username: req.query.username,
       reponame: req.query.reponame,
       targetUrl: targetUrl,
-      data: req.query.data,
       external: req.query.external
     });
   if (!req.query.quick) { res.write('Integrity is OK\n'); res.flush(); }
